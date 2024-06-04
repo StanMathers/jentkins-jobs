31e31947ee65b90f7e3d06ceedf06f8d5e857457
StanMathers <34790002+StanMathers@users.noreply.github.com>
2024-06-04 20:41:28 +0400

    Add: tests

---
+++ tests/test_operations.py
@@ -0,0 +1,14 @@
+def test_addition():
+    assert 1 + 2 == 3
+
+
+def test_subtraction():
+    assert 1 - 2 == -1
+
+
+def test_multiplication():
+    assert 2 * 3 == 6
+
+
+def test_division():
+    assert 3 / 2 == 1.5
